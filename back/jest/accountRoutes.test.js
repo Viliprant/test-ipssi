79d8ee23223dff7560d6f4675500adb516b9d30c
Sébastien Grivel <s.grivel@ecole-ipssi.net>
2021-12-16 11:06:46 +0100

    Ajout des describes

--- back/jest/accountRoutes.test.js
+++ back/jest/accountRoutes.test.js
@@ -1,138 +1,147 @@
 import { get, post, put } from "axios";
 
+describe("Check Account Route", () => {
+    describe("Check Route Get All (/all)", () => {
+        // Vérification de l'accès à la route All si [isAdmin] vaut true.
+        // @return : Accès autorisé.
+        test('Check valid authority', (done) => {
+            get("http://localhost:3000/account/all", {
+                headers: {
+                    isAdmin: true
+                }
+            }).then(response => {
+                expect(response.status).toEqual(200);
+                done()
+            })
+        });
 
-// Vérification de l'accès à la route All si [isAdmin] vaut true.
-// @return : Accès autorisé.
-test('Account Route - Check valid authority', (done) => {
-    get("http://localhost:3000/account/all", {
-        headers: {
-            isAdmin: true
-        }
-    }).then(response => {
-        expect(response.status).toEqual(200);
-        done()
+        // Vérification de l'accès à la route All si [isAdmin] vaut false
+        // @return : Accès refusé.
+        test('Check invalid authority', (done) => {
+            get("http://localhost:3000/account/all", {
+                headers: {
+                    isadmin: false
+                }
+            }).catch((e) => {
+                expect(e.response.status).toEqual(403);
+                done()
+            })
+        });
+
+        // Vérification de l'accès à la route All si [isAdmin] n'ont renseigné.
+        // @return : Accès refusé.
+        test('Check blank authority', (done) => {
+            get("http://localhost:3000/account/all", {
+                headers: {
+                    // No admin property
+                }
+            }).catch((e) => {
+                expect(e.response.status).toEqual(403);
+                done()
+            })
+        });
     })
-});
 
-// Vérification de l'accès à la route All si [isAdmin] vaut false
-// @return : Accès refusé.
-test('Account Route - Check invalid authority', (done) => {
-    get("http://localhost:3000/account/all", {
-        headers: {
-            isadmin: false
-        }
-    }).catch((e) => {
-        expect(e.response.status).toEqual(403);
-        done()
+    describe("Check Route Add Account (/account)", () => {
+        // Ajout d'un nouveau compte.
+        // @return : Account ajouté et test d'un code http 200.
+        test('Check add account', (done) => {
+            const newAccount = {
+                "prenom": "Sayf",
+                "nom": "BEJAOUI",
+                "mdp": "prof",
+                "isAdmin": true
+            }
+
+            const response = post("http://localhost:3000/account", newAccount)
+                .then((response) => {
+                    expect(response.status).toEqual(200);
+                    done()
+                })
+        });
+
+        // Ajout d'un compte sans paramètres.
+        // @return : message d'erreur et code http 400 
+        test('Check add wrong account', (done) => {
+            post("http://localhost:3000/account", {})
+                .catch((e) => {
+                    expect(e.response.status).toEqual(400);
+                    done()
+                })
+        });
     })
-});
 
-// Vérification de l'accès à la route All si [isAdmin] n'ont renseigné.
-// @return : Accès refusé.
-test('Account Route - Check blank authority', (done) => {
-    get("http://localhost:3000/account/all", {
-        headers: {
-            // No admin property
-        }
-    }).catch((e) => {
-        expect(e.response.status).toEqual(403);
-        done()
+    describe("Check Route Get Account (/account/:accountID)", () => {
+        // Récupération d'un compte.
+        // @return : Account attendu et code http 200.
+        test('Check get account', (done) => {
+            const selectedID = "1234FA";
+
+            const expectedAccount = {
+                ID: "1234FA",
+                email: "sarah.pinto@ipssi.net",
+                prenom: "Sarah",
+                nom: "PINTO",
+                isAdmin: true
+            }
+
+            get(`http://localhost:3000/account/${selectedID}`)
+                .then(response => {
+                    expect(response.status).toEqual(200);
+                    expect(response.data).toEqual(expectedAccount);
+                    done()
+                })
+        });
+
+        // Récupération d'un compte avec ID incorrect.
+        // @return : message d'erreur et code http 400 
+        test('Check get wrong ID', (done) => {
+            const selectedID = "1234FAA";
+
+            get(`http://localhost:3000/account/${selectedID}`)
+                .catch(e => {
+                    expect(e.response.status).toEqual(400);
+                    done()
+                })
+        });
     })
-});
 
-// Ajout d'un nouveau compte.
-// @return : Account ajouté et test d'un code http 200.
-test('Account Route - Check add account', (done) => {
-    const newAccount = {
-        "prenom": "Sayf",
-        "nom": "BEJAOUI",
-        "mdp": "prof",
-        "isAdmin": true
-    }
+    describe("Check Route Put Account (/account/:accountID)", () => {
+        // Modifcation d'un compte.
+        // @return : Account attendu et code http 200.
+        test('Check put account', (done) => {
+            const selectedID = {
+                ID: "1234FA"
+            }
 
-    const response = post("http://localhost:3000/account", newAccount)
-        .then((response) => {
-            expect(response.status).toEqual(200);
-            done()
-        })
-});
+            const expectedAccount = {
+                ID: "1234FA",
+                email: "sarah.pinto@ipssi.net",
+                prenom: "Sarah",
+                nom: "PINTO",
+                isAdmin: true
+            }
 
-// Ajout d'un compte sans paramètres.
-// @return : message d'erreur et code http 400 
-test('Account Route - Check add wrong account', (done) => {
-    post("http://localhost:3000/account", {})
-        .catch((e) => {
-            expect(e.response.status).toEqual(400);
-            done()
-        })
-});
+            put(`http://localhost:3000/account/`, selectedID)
+                .then(response => {
+                    expect(response.status).toEqual(200);
+                    expect(response.data).toEqual(expectedAccount);
+                    done()
+                })
+        });
 
-// Récupération d'un compte.
-// @return : Account attendu et code http 200.
-test('Account Route - Check get', (done) => {
-    const selectedID = "1234FA";
+        // Modifcation d'un compte avec ID incorrect.
+        // @return : message d'erreur et code http 400.
+        test('Check put wrong ID', (done) => {
+            const selectedID = {
+                ID: "1234FAAA"
+            }
 
-    const expectedAccount = {
-        ID: "1234FA",
-        email: "sarah.pinto@ipssi.net",
-        prenom: "Sarah",
-        nom: "PINTO",
-        isAdmin: true
-    }
-
-    get(`http://localhost:3000/account/${selectedID}`)
-        .then(response => {
-            expect(response.status).toEqual(200);
-            expect(response.data).toEqual(expectedAccount);
-            done()
-        })
-});
-
-// Récupération d'un compte avec ID incorrect.
-// @return : message d'erreur et code http 400 
-test('Account Route - Check get wrong ID', (done) => {
-    const selectedID = "1234FAA";
-
-    get(`http://localhost:3000/account/${selectedID}`)
-        .catch(e => {
-            expect(e.response.status).toEqual(400);
-            done()
-        })
-});
-
-// Modifcation d'un compte.
-// @return : Account attendu et code http 200.
-test('Account Route - Check put', (done) => {
-    const selectedID = {
-        ID: "1234FA"
-    }
-
-    const expectedAccount = {
-        ID: "1234FA",
-        email: "sarah.pinto@ipssi.net",
-        prenom: "Sarah",
-        nom: "PINTO",
-        isAdmin: true
-    }
-
-    put(`http://localhost:3000/account/`, selectedID)
-        .then(response => {
-            expect(response.status).toEqual(200);
-            expect(response.data).toEqual(expectedAccount);
-            done()
-        })
-});
-
-// Modifcation d'un compte avec ID incorrect.
-// @return : message d'erreur et code http 400.
-test('Account Route - Check put wrong ID', (done) => {
-    const selectedID = {
-        ID: "1234FAAA"
-    }
-
-    put(`http://localhost:3000/account`)
-        .catch(e => {
-            expect(e.response.status).toEqual(400);
-            done()
-        })
-});
+            put(`http://localhost:3000/account`)
+                .catch(e => {
+                    expect(e.response.status).toEqual(400);
+                    done()
+                })
+        });
+    })
+})
--- back/jest/database.test.js
+++ back/jest/database.test.js
@@ -5,207 +5,232 @@ beforeEach(() => {
     FakeDatabase.accounts = [];
 });
 
-// Vérifie la bonne initialisation de la base de donnée.
-// @return : Account[]
+describe("Check FakeDatabase Functions", () => {
+    // Vérifie la bonne initialisation de la base de donnée.
+    // @return : Account[]
+    test('Check initialisation DB', () => {
+        FakeDatabase.init()
 
-test('Check initialisation DB', () => {
-    FakeDatabase.init()
+        const expectedAccount = [
+            {
+                ID: "1234FA",
+                email: "sarah.pinto@ipssi.net",
+                prenom: "Sarah",
+                nom: "PINTO",
+                mdp: "bégé",
+                isAdmin: true
+            },
+            {
+                ID: "234FA",
+                email: "bastien.ederhy@ipssi.net",
+                prenom: "Bastien",
+                nom: "EDERHY",
+                mdp: "hehe",
+                isAdmin: true
+            },
+            {
+                ID: "232A",
+                email: "sebastien.grivel@ipssi.net",
+                prenom: "Sébastien",
+                nom: "GRIVEL",
+                mdp: "haha"
+            },
+            {
+                ID: "23333FA",
+                email: "brandon.soret@ipssi.net",
+                prenom: "Brandon",
+                nom: "SORET",
+                mdp: "hihi",
+                isAdmin: true
+            }
+        ]
 
-    const expectedAccount = [
-        {
-            ID: "1234FA",
-            email: "sarah.pinto@ipssi.net",
-            prenom: "Sarah",
-            nom: "PINTO",
-            mdp: "bégé",
-            isAdmin: true
-        },
-        {
-            ID: "234FA",
-            email: "bastien.ederhy@ipssi.net",
-            prenom: "Bastien",
-            nom: "EDERHY",
-            mdp: "hehe",
-            isAdmin: true
-        },
-        {
-            ID: "232A",
-            email: "sebastien.grivel@ipssi.net",
-            prenom: "Sébastien",
-            nom: "GRIVEL",
-            mdp: "haha"
-        },
-        {
-            ID: "23333FA",
-            email: "brandon.soret@ipssi.net",
-            prenom: "Brandon",
-            nom: "SORET",
-            mdp: "hihi",
-            isAdmin: true
-        }
-    ]
-
-    expect(FakeDatabase.accounts).toEqual(expectedAccount);
-});
-
-// Vérification de l'ajout d'un compte - Paramètres corrects
-// @return : Account
-test("Check add account in database", () => {
-    const newAccount = {
-        email: "brandon.soret@ipssi.net",
-        prenom: "Brandon",
-        nom: "SORET",
-        mdp: "hihi",
-        isAdmin: true
-    }
-
-    const { ID } = FakeDatabase.addAccount(newAccount)
-
-    expect(FakeDatabase.accounts).toEqual([{ ...newAccount, ID }]);
-})
-
-// Vérification de l'ajout d'un compte - [isAdmin] non renseigné (false par défaut).
-// @return : Account (isAdmin : false)
-test("Check add account in database (without being admin)", () => {
-    const newAccount = {
-        email: "brandon.soret@ipssi.net",
-        prenom: "Brandon",
-        nom: "SORET",
-        mdp: "hihi"
-    }
-
-    const expectedNewAccount = {
-        email: "brandon.soret@ipssi.net",
-        prenom: "Brandon",
-        nom: "SORET",
-        mdp: "hihi",
-        isAdmin: false
-    }
-
-    const { ID } = FakeDatabase.addAccount(newAccount)
-
-    expect(FakeDatabase.accounts).toEqual([{ ...expectedNewAccount, ID }]);
-})
-
-// Vérification de la récupération d'un compte.
-// @return : Account
-test("Check get account", () => {
-    FakeDatabase.init();
-    const accountExpected = {
-        ID: "234FA",
-        email: "bastien.ederhy@ipssi.net",
-        prenom: "Bastien",
-        nom: "EDERHY",
-        mdp: "hehe",
-        isAdmin: true
-    };
-
-    const account = FakeDatabase.getAccount("234FA");
-
-    expect(account).toEqual(accountExpected);
-})
-
-// Récupération d'un compte avec un ID inconnu
-// @return : Undefined
-test("Check get bad account", () => {
-    FakeDatabase.init();
-    const account = FakeDatabase.getAccount("234F");
-    expect(account).toBeUndefined();
-})
-
-// Vérification de la modification avec un ID inconnu.
-// @return : Undefined
-test("Check set account", () => {
-    FakeDatabase.init();
-    const accountExpected = {
-        ID: "234FA",
-        email: "sayf.ederhy@ipssi.net",
-        prenom: "Sayf",
-        nom: "EDERHY",
-        mdp: "hehe",
-        isAdmin: true
-    };
-
-    const account = FakeDatabase.setAccount(accountExpected);
-
-    expect(account).toEqual(accountExpected);
-})
-
-// Modification d'un compte - ID inconnu.
-// @return : Undefined
-test("Check set bad account", () => {
-    FakeDatabase.init();
-    const accountExpected = {
-        ID: "234FA45",
-        email: "sayf.ederhy@ipssi.net",
-        prenom: "Sayf",
-        nom: "EDERHY",
-        mdp: "hehe",
-        isAdmin: true
-    };
-
-    const account = FakeDatabase.setAccount(accountExpected);
-
-    expect(account).toBeUndefined();
-})
-
-// Vérification de la modification d'un compte en renseignant que l'ID
-// @return : Account (non modifié)
-test("Check set only ID account", () => {
-    FakeDatabase.init();
-    const accountExpected = {
-        ID: "234FA",
-        email: "bastien.ederhy@ipssi.net",
-        prenom: "Bastien",
-        nom: "EDERHY",
-        mdp: "hehe",
-        isAdmin: true
-    };
-
-    const account = FakeDatabase.setAccount({ ID: "234FA" });
-
-    expect(account).toEqual(accountExpected);
-})
-
-// Vérification de la modification sans renseigner de paramètres.
-// @return : Undefined
-test("Check set without ID account", () => {
-    FakeDatabase.init();
-
-    const account = FakeDatabase.setAccount();
-
-    expect(account).toBeUndefined();
-})
-
-describe("Check regex", () => {
-    test("Check with valid email", () => {
-        const email = "brandon.soret@ipssi.net"
-        const response = FakeDatabase.validateEmail(email);
-        expect(response).toBeTruthy();
-
+        expect(FakeDatabase.accounts).toEqual(expectedAccount);
     });
-    test("Check without @ ", () => {
-        const email = "brandon.soretipssi.net"
-        const response = FakeDatabase.validateEmail(email);
-        expect(response).toBeFalsy();
 
-    });
-    test("Check without domain ", () => {
-        const email = "brandon.soretipssi."
-        const response = FakeDatabase.validateEmail(email);
-        expect(response).toBeFalsy();
 
-    });
-    test("Check with many @ ", () => {
-        const email = "br@ndon.soret@ipssi.net"
-        const response = FakeDatabase.validateEmail(email);
-        expect(response).toBeFalsy();
+    describe("Function Add", () => {
+        // Vérification de l'ajout d'un compte - Paramètres corrects
+        // @return : Account
+        test("Check add account in database", () => {
+            const newAccount = {
+                email: "brandon.soret@ipssi.net",
+                prenom: "Brandon",
+                nom: "SORET",
+                mdp: "hihi",
+                isAdmin: true
+            }
 
-    });
-    test("Check with many . ", () => {
-        const email = "brandon.soret.adore.lipssi@sarca.sme.net"
-        const response = FakeDatabase.validateEmail(email);
-        expect(response).toBeTruthy();
+            const { ID } = FakeDatabase.addAccount(newAccount)
 
+            expect(FakeDatabase.accounts).toEqual([{ ...newAccount, ID }]);
+        })
+
+        // Vérification de l'ajout d'un compte - [isAdmin] non renseigné (false par défaut).
+        // @return : Account (isAdmin : false)
+        test("Check add account in database (without being admin)", () => {
+            const newAccount = {
+                email: "brandon.soret@ipssi.net",
+                prenom: "Brandon",
+                nom: "SORET",
+                mdp: "hihi"
+            }
+
+            const expectedNewAccount = {
+                email: "brandon.soret@ipssi.net",
+                prenom: "Brandon",
+                nom: "SORET",
+                mdp: "hihi",
+                isAdmin: false
+            }
+
+            const { ID } = FakeDatabase.addAccount(newAccount)
+
+            expect(FakeDatabase.accounts).toEqual([{ ...expectedNewAccount, ID }]);
+        })
     });
+
+    describe("Function Get", () => {
+        // Vérification de la récupération d'un compte.
+        // @return : Account
+        test("Check get account", () => {
+            FakeDatabase.init();
+            const accountExpected = {
+                ID: "234FA",
+                email: "bastien.ederhy@ipssi.net",
+                prenom: "Bastien",
+                nom: "EDERHY",
+                mdp: "hehe",
+                isAdmin: true
+            };
+
+            const account = FakeDatabase.getAccount("234FA");
+
+            expect(account).toEqual(accountExpected);
+        })
+
+        // Récupération d'un compte avec un ID inconnu
+        // @return : Undefined
+        test("Check get bad account", () => {
+            FakeDatabase.init();
+            const account = FakeDatabase.getAccount("234F");
+            expect(account).toBeUndefined();
+        })
+    })
+
+    describe("Function Set", () => {
+
+        // Vérification de la modification avec un ID inconnu.
+        // @return : Undefined
+        test("Check set account", () => {
+            FakeDatabase.init();
+            const accountExpected = {
+                ID: "234FA",
+                email: "sayf.ederhy@ipssi.net",
+                prenom: "Sayf",
+                nom: "EDERHY",
+                mdp: "hehe",
+                isAdmin: true
+            };
+
+            const account = FakeDatabase.setAccount(accountExpected);
+
+            expect(account).toEqual(accountExpected);
+        })
+
+        // Modification d'un compte - ID inconnu.
+        // @return : Undefined
+        test("Check set bad account", () => {
+            FakeDatabase.init();
+            const accountExpected = {
+                ID: "234FA45",
+                email: "sayf.ederhy@ipssi.net",
+                prenom: "Sayf",
+                nom: "EDERHY",
+                mdp: "hehe",
+                isAdmin: true
+            };
+
+            const account = FakeDatabase.setAccount(accountExpected);
+
+            expect(account).toBeUndefined();
+        })
+
+        // Vérification de la modification d'un compte en renseignant que l'ID
+        // @return : Account (non modifié)
+        test("Check set only ID account", () => {
+            FakeDatabase.init();
+            const accountExpected = {
+                ID: "234FA",
+                email: "bastien.ederhy@ipssi.net",
+                prenom: "Bastien",
+                nom: "EDERHY",
+                mdp: "hehe",
+                isAdmin: true
+            };
+
+            const account = FakeDatabase.setAccount({ ID: "234FA" });
+
+            expect(account).toEqual(accountExpected);
+        })
+
+        // Vérification de la modification sans renseigner de paramètres.
+        // @return : Undefined
+        test("Check set without ID account", () => {
+            FakeDatabase.init();
+
+            const account = FakeDatabase.setAccount();
+
+            expect(account).toBeUndefined();
+        })
+    })
+
+    // Liste de test regex email
+    describe("Check Email Regex", () => {
+        // Valide email
+        // @return : True
+        test("Check with valid email", () => {
+            const email = "brandon.soret@ipssi.net"
+            const response = FakeDatabase.validateEmail(email);
+            expect(response).toBeTruthy();
+
+        });
+
+        // Email sans @
+        // @return : False
+        test("Check without @ ", () => {
+            const email = "brandon.soretipssi.net"
+            const response = FakeDatabase.validateEmail(email);
+            expect(response).toBeFalsy();
+
+        });
+
+        // Email sans nom de domaine
+        // @return : False
+        test("Check without domain ", () => {
+            const email = "brandon.soret@"
+            const response = FakeDatabase.validateEmail(email);
+            expect(response).toBeFalsy();
+
+        });
+
+        // Email avec plusieurs @
+        // @return : False
+        test("Check with many @ ", () => {
+            const email = "br@ndon.soret@ipssi.net"
+            const response = FakeDatabase.validateEmail(email);
+            expect(response).toBeFalsy();
+
+        });
+
+        // Email avec plusieurs .
+        // @return : True
+        test("Check with many . ", () => {
+            const email = "brandon.soret.adore.lipssi@sarca.sme.net"
+            const response = FakeDatabase.validateEmail(email);
+            expect(response).toBeTruthy();
+
+        });
+    })
+
 })
